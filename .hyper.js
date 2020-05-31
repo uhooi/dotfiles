d7b490c7602b183fcf4bc09ebaf91c4aefc9f5ff
uhooi <uhooi@server.fake>
2020-05-31 19:14:28 +0900

    Hyperのシェルを明示的にbashにした

--- .hyper.js
+++ .hyper.js
@@ -97,7 +97,7 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: '/bin/bash',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
