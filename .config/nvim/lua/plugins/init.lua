7755a477990efa31f98a24454bcaed6e49743db5
uhooi <the_uhooi@yahoo.co.jp>
2022-08-12 12:06:51 +0900

    Setup packer.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,2 +1,40 @@
-print('plugins')
+-- packer.nvim
+
+-- ref: https://github.com/wbthomason/packer.nvim/blob/afab89594f4f702dc3368769c95b782dbdaeaf0a/README.md
+--    : https://github.com/delphinus/dotfiles/blob/e562d4f8e99793e6ae1cd330c9208dac1d29d407/.config/nvim/lua/core/pack/load.lua
+--    : https://github.com/delphinus/dotfiles/blob/e562d4f8e99793e6ae1cd330c9208dac1d29d407/.config/nvim/lua/modules/opt/init.lua
+--    : https://github.com/craftzdog/dotfiles-public/blob/50344f53c96c241e6d0659ed19507c70771bd971/.config/nvim/lua/plugins.lua
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- Colorscheme
+  use {
+    'cocopon/iceberg.vim',
+    config = function()
+      vim.cmd([[
+        set background=dark
+        autocmd VimEnter * ++nested colorscheme iceberg
+      ]])
+    end,
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
 
