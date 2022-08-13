460a15991140ff79aa64f3e75bb2fba51057bf67
uhooi <the_uhooi@yahoo.co.jp>
2022-08-13 15:49:18 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,11 @@
+vim.scriptencoding = 'utf-8'
+
+-- ref: https://github.com/wbthomason/dotfiles/blob/08e10d9cc8d2c5bdd2f947caa2c40206efde8db7/neovim/.config/nvim/init.lua
+--    : https://github.com/craftzdog/dotfiles-public/blob/50344f53c96c241e6d0659ed19507c70771bd971/.config/nvim/lua/base.lua
+
+vim.g.mapleader = [[,]]
+vim.keymap.set('', '\\', ',', { noremap = true })
+
 require('basics')
 require('filetypes')
 require('mappings')
