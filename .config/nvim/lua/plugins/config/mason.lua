4c9f24af0745ec7f44fe14e8195feade8791745a
uhooi <the_uhooi@yahoo.co.jp>
2022-08-14 11:57:55 +0900

    Update Neovim LSP setting

--- .config/nvim/lua/plugins/config/mason.lua
+++ .config/nvim/lua/plugins/config/mason.lua
@@ -1,9 +1,9 @@
 require('mason').setup {
   ui = {
     icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
+      package_installed = '✓',
+      package_pending = '➜',
+      package_uninstalled = '✗',
     },
   },
 }
@@ -11,14 +11,33 @@ require('mason').setup {
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers {
+  -- FIXME: Not work
   function(server_name)
-    local capabilities = require('cmp_nvim_lsp')
-      .update_capabilities(
+    local opts = {
+      capabilities = require('cmp_nvim_lsp').update_capabilities(
         vim.lsp.protocol.make_client_capabilities()
       )
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
     }
+    if server_name == 'sumneko_lua' then
+      -- ref: https://github.com/neovim/nvim-lspconfig/blob/da7461b596d70fa47b50bf3a7acfaef94c47727d/doc/server_configurations.md#sumneko_lua
+      opts.settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file('', true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      }
+    end
+    require('lspconfig')[server_name].setup(opts)
   end
 }
 
