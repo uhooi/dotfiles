d7ec1d3c7c4c2dd3d071523d6aaf5785adc5681a
uhooi <the_uhooi@yahoo.co.jp>
2022-08-14 23:07:32 +0900

    Add nvim-cmp keymap

--- .config/nvim/lua/plugins/config/nvim_cmp.lua
+++ .config/nvim/lua/plugins/config/nvim_cmp.lua
@@ -1,5 +1,6 @@
 -- ref: https://github.com/hrsh7th/nvim-cmp/wiki/Language-Server-Specific-Samples
 --    : https://github.com/hrsh7th/dotfiles/blob/477047a6280c2c1f858e94e9073fd20bab05c0c7/.config/nvim/init.vim#L767-L852
+--    : https://github.com/hrsh7th/nvim-cmp/blob/f51dc68e1bb170fc49c2d7e13eb45e5ec83f5ee9/lua/cmp/config/mapping.lua
 --    : https://zenn.dev/botamotch/articles/21073d78bc68bf
 
 local cmp = require('cmp')
@@ -26,6 +27,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-l>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
   sources = cmp.config.sources {
