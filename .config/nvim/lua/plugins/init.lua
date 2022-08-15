58c89599047be85f1d275361f1a528abf7bcc74c
uhooi <the_uhooi@yahoo.co.jp>
2022-08-16 00:18:24 +0900

    Refactor plugins/init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -7,11 +7,12 @@
 --    : https://github.com/delphinus/dotfiles/blob/e562d4f8e99793e6ae1cd330c9208dac1d29d407/.config/nvim/lua/modules/opt/init.lua
 --    : https://github.com/craftzdog/dotfiles-public/blob/50344f53c96c241e6d0659ed19507c70771bd971/.config/nvim/lua/plugins.lua
 
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path })
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({
+    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
+  })
   vim.cmd [[packadd packer.nvim]]
 end
 
@@ -60,7 +61,7 @@ return require('packer').startup(function(use)
 
   use {
     'numToStr/FTerm.nvim',
-    config = function () require('plugins.config.fterm') end,
+    config = function() require('plugins.config.fterm') end,
   }
 
   use {
