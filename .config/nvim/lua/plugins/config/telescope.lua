c8f522e4b829accc27d3f3db3ee9cbb3c007ade2
uhooi <the_uhooi@yahoo.co.jp>
2022-08-15 15:24:19 +0900

    Update telescope keymaps

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -4,8 +4,8 @@
 require('telescope').load_extension('fzf')
 
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<Leader>ff', telescope_builtin.find_files, { noremap = true })
-vim.keymap.set('n', '<Leader>fg', telescope_builtin.live_grep, { noremap = true })
-vim.keymap.set('n', '<Leader>fb', telescope_builtin.buffers, { noremap = true })
-vim.keymap.set('n', '<Leader>fh', telescope_builtin.help_tags, { noremap = true })
-vim.keymap.set('n', '<Leader>fp', telescope_builtin.planets, { noremap = true })
+vim.keymap.set('n', '<Leader>f', telescope_builtin.find_files, { noremap = true })
+vim.keymap.set('n', '<Leader>g', telescope_builtin.live_grep, { noremap = true })
+vim.keymap.set('n', '<Leader>b', telescope_builtin.buffers, { noremap = true })
+vim.keymap.set('n', '<Leader>h', telescope_builtin.help_tags, { noremap = true })
+vim.keymap.set('n', '<Leader>p', telescope_builtin.planets, { noremap = true })
