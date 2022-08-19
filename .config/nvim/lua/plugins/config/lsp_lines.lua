3670ab30cbf5fc8a79e8d9c24f03f0a45d2602f1
uhooi <the_uhooi@yahoo.co.jp>
2022-08-19 21:23:35 +0900

    Update lsp_lines keymap

--- .config/nvim/lua/plugins/config/lsp_lines.lua
+++ .config/nvim/lua/plugins/config/lsp_lines.lua
@@ -1,11 +1,9 @@
-require('lsp_lines').setup()
+local lsp_lines = require('lsp_lines')
+
+lsp_lines.setup()
+
 vim.diagnostic.config {
   virtual_text = false,
 }
-vim.keymap.set(
-  '',
-  '<Leader>l',
-  require('lsp_lines').toggle,
-  { desc = 'Toggle lsp_lines' }
-)
 
+vim.keymap.set('n', '<Leader>l', lsp_lines.toggle, { desc = 'Toggle lsp_lines' })
