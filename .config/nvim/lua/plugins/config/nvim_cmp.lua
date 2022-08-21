e5e77d6db11b68716521dcbb682d44e29d59a411
uhooi <the_uhooi@yahoo.co.jp>
2022-08-21 18:39:31 +0900

    Update nvim-cmp config

--- .config/nvim/lua/plugins/config/nvim_cmp.lua
+++ .config/nvim/lua/plugins/config/nvim_cmp.lua
@@ -22,8 +22,8 @@ cmp.setup {
     end,
   },
   window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert {
     ['<S-Tab>'] = cmp.mapping.select_prev_item(),
