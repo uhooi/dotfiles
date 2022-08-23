d113f82235fa4810dee094d1368abee1d4f9a2a7
uhooi <the_uhooi@yahoo.co.jp>
2022-08-23 12:27:10 +0900

    Remove noremap = true

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,7 @@ vim.scriptencoding = 'utf-8'
 --    : https://github.com/craftzdog/dotfiles-public/blob/50344f53c96c241e6d0659ed19507c70771bd971/.config/nvim/lua/base.lua
 
 vim.g.mapleader = [[,]]
-vim.keymap.set('', '\\', ',', { noremap = true })
+vim.keymap.set('', '\\', ',')
 
 require('basics')
 require('filetypes')
