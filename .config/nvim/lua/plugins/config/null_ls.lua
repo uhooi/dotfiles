12c143ad0b1e148bea0e86acea8fdbb79a427636
uhooi <the_uhooi@yahoo.co.jp>
2022-08-24 12:31:24 +0900

    Add eslint

--- .config/nvim/lua/plugins/config/null_ls.lua
+++ .config/nvim/lua/plugins/config/null_ls.lua
@@ -1,6 +1,6 @@
 -- ref: https://github.com/delphinus/dotfiles/blob/e562d4f8e99793e6ae1cd330c9208dac1d29d407/.config/nvim/lua/modules/lsp/config.lua#L440-L665
 
-local null_ls = require("null-ls")
+local null_ls = require('null-ls')
 
 null_ls.setup {
   on_attach = function(_, bufnr)
@@ -22,14 +22,22 @@ null_ls.setup {
     vim.keymap.set('n', 'gf', vim.lsp.buf.formatting, bufopts)
   end,
   sources = {
-    -- ref: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/8c90ccf7ffbdeb49da415837ec45e6ac457d5c60/doc/BUILTINS.md#textlint
+    -- ref: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/8c90ccf7ffbdeb49da415837ec45e6ac457d5c60/doc/BUILTINS.md
     --    : https://github.com/jose-elias-alvarez/null-ls.nvim/blob/8c90ccf7ffbdeb49da415837ec45e6ac457d5c60/doc/BUILTIN_CONFIG.md#using-local-executables
     --    : https://zenn.dev/koga1020/scraps/46a160c9aec061
+    null_ls.builtins.code_actions.eslint.with {
+      prefer_local = 'node_modules/.bin',
+    },
+    null_ls.builtins.diagnostics.eslint.with {
+      prefer_local = 'node_modules/.bin',
+    },
+    null_ls.builtins.formatting.eslint.with {
+      prefer_local = 'node_modules/.bin',
+    },
     null_ls.builtins.diagnostics.textlint.with {
       filetypes = { 'markdown' },
       prefer_local = 'node_modules/.bin',
     },
-    -- ref: https://github.com/jose-elias-alvarez/null-ls.nvim/pull/1013
     null_ls.builtins.formatting.textlint.with {
       filetypes = { 'markdown' },
       prefer_local = 'node_modules/.bin',
