cad3703f3111b576f129dcd0118a393d23126b80
uhooi <the_uhooi@yahoo.co.jp>
2022-11-22 19:23:11 +0900

    Update noice.nvim config

--- .config/nvim/lua/plugins/config/noice.lua
+++ .config/nvim/lua/plugins/config/noice.lua
@@ -2,10 +2,17 @@ vim.opt.cmdheight = 0
 
 require('noice').setup {
   cmdline = {
-    icons = {
-      ["/"] = { icon = " ", hl_group = "DiagnosticWarn" },
-      ["?"] = { icon = " ", hl_group = "DiagnosticWarn" },
-      [":"] = { icon = " ", hl_group = "DiagnosticInfo", firstc = false },
+    format = {
+      cmdline = { pattern = '^:', icon = ' ', lang = 'vim' },
+      search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+      search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+    },
+  },
+  lsp = {
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true,
     },
   },
 }
