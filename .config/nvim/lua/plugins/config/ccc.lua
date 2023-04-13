4501161115b853ad8119b90e00edbaa7d7ef000d
uhooi <the_uhooi@yahoo.co.jp>
2023-04-13 16:25:52 +0900

    Update ccc.nvim settings

--- .config/nvim/lua/plugins/config/ccc.lua
+++ .config/nvim/lua/plugins/config/ccc.lua
@@ -1,7 +1,19 @@
 vim.opt.termguicolors = true
 
-require('ccc').setup {
+local ccc = require('ccc')
+ccc.setup {
   highlighter = {
     auto_enable = true,
+  },
+  pickers = {
+    ccc.picker.hex,
+    ccc.picker.css_rgb,
+    ccc.picker.css_hsl,
+    ccc.picker.css_hwb,
+    ccc.picker.css_lab,
+    ccc.picker.css_lch,
+    ccc.picker.css_oklab,
+    ccc.picker.css_oklch,
+    ccc.picker.css_name,
   },
 }
