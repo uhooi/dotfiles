9ed687fc14ccefca0a5e77dfce4551b9562f03cc
uhooi <the_uhooi@yahoo.co.jp>
2023-05-13 23:16:27 +0900

    Setup cmp-emoji

--- .config/nvim/lua/plugins/config/nvim_cmp.lua
+++ .config/nvim/lua/plugins/config/nvim_cmp.lua
@@ -41,6 +41,7 @@ cmp.setup {
     { name = 'nvim_lsp_signature_help' },
     { name = 'path' },
     { name = 'nvim_lsp' },
+    { name = 'emoji' },
     { name = 'vsnip' },
     { name = 'buffer' },
   },
