63401b69825be9af1b964dd2c0eaae11a21beacf
uhooi <the_uhooi@yahoo.co.jp>
2023-05-28 17:52:38 +0900

    Disable lua_ls checkThirdParty

--- .config/nvim/lua/plugins/config/mason.lua
+++ .config/nvim/lua/plugins/config/mason.lua
@@ -49,6 +49,7 @@ mason_lspconfig.setup_handlers {
           },
           workspace = {
             library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = false,
           },
           telemetry = {
             enable = false,
