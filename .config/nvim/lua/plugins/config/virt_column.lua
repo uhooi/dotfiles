86655f311c936004cb5c529b9f76fc298b1b18c8
uhooi <the_uhooi@yahoo.co.jp>
2023-05-30 14:46:00 +0900

    Refactor virt-column config

---
+++ .config/nvim/lua/plugins/config/virt_column.lua
@@ -0,0 +1,3 @@
+require('virt-column').setup {
+  char = '│', -- ref: https://github.com/lukas-reineke/indent-blankline.nvim/blob/7075d7861f7a6bbf0de0298c83f8a13195e6ec01/doc/indent_blankline.txt#L159
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -49,9 +49,7 @@ require('lazy').setup({
   {
     'lukas-reineke/virt-column.nvim',
     config = function()
-      require('virt-column').setup {
-        char = '│', -- ref: https://github.com/lukas-reineke/indent-blankline.nvim/blob/7075d7861f7a6bbf0de0298c83f8a13195e6ec01/doc/indent_blankline.txt#L159
-      }
+      require('plugins.config.virt_column')
     end,
   },
 
