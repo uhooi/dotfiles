5ff2a07d864473a9dedb6330f18d1b548421ee36
uhooi <the_uhooi@yahoo.co.jp>
2023-05-30 14:30:39 +0900

    Add comment

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -50,7 +50,7 @@ require('lazy').setup({
     'lukas-reineke/virt-column.nvim',
     config = function()
       require('virt-column').setup {
-        char = '│',
+        char = '│', -- ref: https://github.com/lukas-reineke/indent-blankline.nvim/blob/7075d7861f7a6bbf0de0298c83f8a13195e6ec01/doc/indent_blankline.txt#L159
       }
     end,
   },
