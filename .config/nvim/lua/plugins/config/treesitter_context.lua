ade38b7e18a5d9d398425fde21d2d3dca4a6971f
uhooi <the_uhooi@yahoo.co.jp>
2023-05-31 17:22:24 +0900

    Format

--- .config/nvim/lua/plugins/config/treesitter_context.lua
+++ .config/nvim/lua/plugins/config/treesitter_context.lua
@@ -1,6 +1,6 @@
 local treesitter_context = require('treesitter-context')
 
-treesitter_context.setup()
+treesitter_context.setup {}
 
 vim.keymap.set('n', '[c', function()
   treesitter_context.go_to_context()
