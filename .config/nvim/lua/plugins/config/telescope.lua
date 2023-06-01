e4d64e3c0e16d988b0fd3d2b5e54bf0d5ed33326
uhooi <the_uhooi@yahoo.co.jp>
2023-06-01 15:30:08 +0900

    Update telescope.lua

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -8,7 +8,7 @@ telescope.load_extension('ui-select')
 --telescope.load_extension('noice')
 
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<Leader>f', telescope_builtin.find_files)
+vim.keymap.set('n', '<Leader>f', telescope_builtin.git_files)
 vim.keymap.set('n', '<Leader>g', telescope_builtin.live_grep)
 vim.keymap.set('n', '<Leader>b', telescope_builtin.buffers)
 vim.keymap.set('n', '<Leader>h', telescope_builtin.help_tags)
