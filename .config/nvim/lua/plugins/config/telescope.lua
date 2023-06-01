af59da845d89fa3b75bb810835434d3c9a483ad1
uhooi <the_uhooi@yahoo.co.jp>
2023-06-01 16:08:50 +0900

    Live grep from project git root

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -7,9 +7,28 @@ telescope.load_extension('ui-select')
 -- FIXME: Error
 --telescope.load_extension('noice')
 
+-- ref: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#live-grep-from-project-git-root-with-fallback
+local function live_grep_from_project_git_root()
+  local function is_git_repo()
+    vim.fn.system('git rev-parse --is-inside-work-tree')
+    return vim.v.shell_error == 0
+  end
+
+  local function get_git_root()
+    return print(vim.fn.system('git rev-parse --show-superproject-working-tree --show-toplevel | head -1'))
+  end
+
+  local opts = {}
+  if is_git_repo() then
+    opts = { cwd = get_git_root() }
+  end
+
+  require('telescope.builtin').live_grep(opts)
+end
+
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<Leader>f', telescope_builtin.git_files)
-vim.keymap.set('n', '<Leader>g', telescope_builtin.live_grep)
+vim.keymap.set('n', '<Leader>g', live_grep_from_project_git_root)
 vim.keymap.set('n', '<Leader>b', telescope_builtin.buffers)
 vim.keymap.set('n', '<Leader>h', telescope_builtin.help_tags)
 vim.keymap.set('n', '<Leader>n', '<Cmd>Noice telescope<CR>')
