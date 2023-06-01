6ff2cfba2afbf0b345449c055c7291c27a00b6b0
uhooi <the_uhooi@yahoo.co.jp>
2023-06-01 23:13:52 +0900

    Format comment

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -4,8 +4,7 @@
 local telescope = require('telescope')
 telescope.load_extension('fzf')
 telescope.load_extension('ui-select')
--- FIXME: Error
---telescope.load_extension('noice')
+-- telescope.load_extension('noice') -- FIXME: Error
 
 -- ref: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#mapping-esc-to-quit-in-insert-mode
 local telescope_actions = require('telescope.actions')
