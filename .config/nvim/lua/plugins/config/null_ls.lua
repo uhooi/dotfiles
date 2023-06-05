b05c46fabc0cd2a08f27c4175e94138e0e7f5d5f
uhooi <the_uhooi@yahoo.co.jp>
2023-06-05 15:49:33 +0900

    Remove unnecessary keymaps

--- .config/nvim/lua/plugins/config/null_ls.lua
+++ .config/nvim/lua/plugins/config/null_ls.lua
@@ -3,26 +3,6 @@
 local null_ls = require('null-ls')
 
 null_ls.setup {
-  on_attach = function(_, bufnr)
-    local bufopts = { buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', 'gwa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', 'gwr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', 'gwl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', 'gn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', 'gf', function()
-      vim.lsp.buf.format { async = true }
-    end, bufopts)
-  end,
   sources = {
     -- ref: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/8c90ccf7ffbdeb49da415837ec45e6ac457d5c60/doc/BUILTINS.md
     --    : https://github.com/jose-elias-alvarez/null-ls.nvim/blob/8c90ccf7ffbdeb49da415837ec45e6ac457d5c60/doc/BUILTIN_CONFIG.md#using-local-executables
