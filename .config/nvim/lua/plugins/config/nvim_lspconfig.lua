dbcce364443f22877e7c3ab7755b24a05c06a8d7
uhooi <the_uhooi@yahoo.co.jp>
2023-06-14 02:51:32 +0900

    Fix border

--- .config/nvim/lua/plugins/config/nvim_lspconfig.lua
+++ .config/nvim/lua/plugins/config/nvim_lspconfig.lua
@@ -58,19 +58,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gf', function()
       vim.lsp.buf.format { async = true }
     end, bufopts)
+
+    -- ref: https://vi.stackexchange.com/questions/39074/user-borders-around-lsp-floating-windows
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = 'rounded',
+    })
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = 'rounded',
+    })
+    vim.diagnostic.config {
+      float = { border = 'rounded' },
+    }
   end,
 })
-
--- FIXME: Not work
--- ref: https://vi.stackexchange.com/questions/39074/user-borders-around-lsp-floating-windows
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'rounded',
-})
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = 'rounded',
-})
-vim.diagnostic.config {
-  float = { border = 'rounded' },
-}
 
 require('lspconfig.ui.windows').default_options.border = 'rounded'
