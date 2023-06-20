d0d98c19b6c0bfda991cb2ead4d6c5e209aeb8eb
uhooi <the_uhooi@yahoo.co.jp>
2023-06-20 21:38:12 +0900

    Update skkeleton config

--- .config/nvim/lua/plugins/config/skkeleton.lua
+++ .config/nvim/lua/plugins/config/skkeleton.lua
@@ -2,10 +2,10 @@
 --    : https://github.com/uga-rosa/dotfiles/blob/60bb2d9c3d9f3b0d23138de6cda2b7d24f02ea3d/nvim/lua/rc/plugins/skk.lua
 
 local dictionaries_dir = '~/.config/nvim/lua/plugins/config/dictionaries/skk/'
-local temp_dir = '/tmp/.skkeleton/'
+local skkeleton_dir = '~/.skkeleton/'
 
 vim.fn['skkeleton#config'] {
-  completionRankFile = temp_dir .. 'completion_rank.txt',
+  completionRankFile = skkeleton_dir .. 'completion_rank.txt',
   eggLikeNewline = true,
   globalDictionaries = {
     dictionaries_dir .. 'SKK-JISYO.L',
@@ -17,6 +17,8 @@ vim.fn['skkeleton#config'] {
   markerHenkan = '▽ ',
   markerHenkanSelect = '▼ ',
   registerConvertResult = true,
+  showCandidatesCount = 1,
+  userJisyo = skkeleton_dir .. 'user_jisyo.txt',
 }
 
 vim.keymap.set({ 'i', 'c' }, '<C-j>', '<Plug>(skkeleton-toggle)')
