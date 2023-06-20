74467002623751df99e0c98ce1f789c55ce2ed2b
uhooi <the_uhooi@yahoo.co.jp>
2023-06-20 17:33:10 +0900

    Add SKK text

--- .config/nvim/lua/plugins/config/skkeleton_indicator.lua
+++ .config/nvim/lua/plugins/config/skkeleton_indicator.lua
@@ -1,1 +1,7 @@
-require('skkeleton_indicator').setup()
+require('skkeleton_indicator').setup {
+  eijiText = 'ASCII',
+  hiraText = 'かな',
+  kataText = 'カナ',
+  hankataText = '半ｶﾅ',
+  zenkakuText = '全英',
+}
