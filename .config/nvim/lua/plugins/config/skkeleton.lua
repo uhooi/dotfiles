630218528da516d8b37e676f1cfcf9d804565928
uhooi <the_uhooi@yahoo.co.jp>
2023-06-20 10:52:35 +0900

    Rename

--- .config/nvim/lua/plugins/config/skkeleton.lua
+++ .config/nvim/lua/plugins/config/skkeleton.lua
@@ -1,18 +1,18 @@
 -- ref: https://github.com/delphinus/dotfiles/blob/3dd1d9304114983a7ad00a3a04e964c861a99425/.config/nvim/lua/modules/cmp/config.lua
 --    : https://github.com/uga-rosa/dotfiles/blob/60bb2d9c3d9f3b0d23138de6cda2b7d24f02ea3d/nvim/lua/rc/plugins/skk.lua
 
-local skk_dictionaries_dir = '~/.config/nvim/lua/plugins/config/dictionaries/skk/'
-local skk_temp_dir = '/tmp/.skkeleton/'
+local dictionaries_dir = '~/.config/nvim/lua/plugins/config/dictionaries/skk/'
+local temp_dir = '/tmp/.skkeleton/'
 
 vim.fn['skkeleton#config'] {
-  completionRankFile = skk_temp_dir .. 'completion_rank.txt',
+  completionRankFile = temp_dir .. 'completion_rank.txt',
   eggLikeNewline = true,
   globalDictionaries = {
-    skk_dictionaries_dir .. 'SKK-JISYO.L',
-    skk_dictionaries_dir .. 'SKK-JISYO.jinmei',
-    skk_dictionaries_dir .. 'SKK-JISYO.geo',
-    skk_dictionaries_dir .. 'SKK-JISYO.station',
-    skk_dictionaries_dir .. 'SKK-JISYO.propernoun',
+    dictionaries_dir .. 'SKK-JISYO.L',
+    dictionaries_dir .. 'SKK-JISYO.jinmei',
+    dictionaries_dir .. 'SKK-JISYO.geo',
+    dictionaries_dir .. 'SKK-JISYO.station',
+    dictionaries_dir .. 'SKK-JISYO.propernoun',
   },
   markerHenkan = '▽ ',
   markerHenkanSelect = '▼ ',
