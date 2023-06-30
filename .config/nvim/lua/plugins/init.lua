1b5da1cdbfaaa9e2d4cff1d7e61dd87fc46e8843
uhooi <the_uhooi@yahoo.co.jp>
2023-06-30 21:45:27 +0900

    Setup dial.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -135,6 +135,21 @@ require('lazy').setup({
   { 'tomtom/tcomment_vim' },
 
   {
+    'monaqa/dial.nvim',
+    config = function()
+      local dial_map = require('dial.map')
+      vim.keymap.set('n', '+', dial_map.inc_normal())
+      vim.keymap.set('n', '-', dial_map.dec_normal())
+      vim.keymap.set('n', 'g+', dial_map.inc_gnormal())
+      vim.keymap.set('n', 'g-', dial_map.dec_gnormal())
+      vim.keymap.set('v', '+', dial_map.inc_visual())
+      vim.keymap.set('v', '-', dial_map.dec_visual())
+      vim.keymap.set('v', 'g+', dial_map.inc_gvisual())
+      vim.keymap.set('v', 'g-', dial_map.dec_gvisual())
+    end,
+  },
+
+  {
     'github/copilot.vim',
     config = function()
       require('plugins.config.copilot')
