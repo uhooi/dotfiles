2fc33293f931005f18d2e9133263e6f18f7fd41f
uhooi <the_uhooi@yahoo.co.jp>
2023-07-03 11:43:45 +0900

    Add border to skkeleton indicator

--- .config/nvim/lua/plugins/config/skkeleton_indicator.lua
+++ .config/nvim/lua/plugins/config/skkeleton_indicator.lua
@@ -1,10 +1,16 @@
 local skk_utils = require('plugins.config.shared.skk_utils')
 
+vim.api.nvim_set_hl(0, 'SkkeletonIndicatorEiji', { fg = '#88c0d0', bg = 'none', bold = true })
+vim.api.nvim_set_hl(0, 'SkkeletonIndicatorHira', { fg = '#a3be8c', bg = 'none', bold = true })
+vim.api.nvim_set_hl(0, 'SkkeletonIndicatorKata', { fg = '#ebcb8b', bg = 'none', bold = true })
+vim.api.nvim_set_hl(0, 'SkkeletonIndicatorHankata', { fg = '#b48ead', bg = 'none', bold = true })
+vim.api.nvim_set_hl(0, 'SkkeletonIndicatorZenkaku', { fg = '#88c0d0', bg = 'none', bold = true })
+
 require('skkeleton_indicator').setup {
   eijiText = skk_utils.mode_texts.ascii,
   hiraText = skk_utils.mode_texts.hira,
   kataText = skk_utils.mode_texts.kata,
   hankataText = skk_utils.mode_texts.hankata,
   zenkakuText = skk_utils.mode_texts.zenkaku,
-  -- border = 'rounded',
+  border = 'rounded',
 }
