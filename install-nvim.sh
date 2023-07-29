a82d12792cfc57955a5d0564519445c2ae1c2fed
uhooi <the_uhooi@yahoo.co.jp>
2023-07-29 17:24:24 +0900

    install-nvim.sh を更新

--- install-nvim.sh
+++ install-nvim.sh
@@ -9,14 +9,16 @@
 # ----
 
 CURRENT_DIR=`dirname $0`
-NVIM_ROOT=~/ghq/github.com/neovim/neovim
+NVIM_ROOT=${HOME}/ghq/github.com/neovim/neovim
 
 cd ${NVIM_ROOT}
 
 git fetch --prune origin
 git rebase origin/master
 
-make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local/nvim
+make \
+  CMAKE_BUILD_TYPE=Release \
+  CMAKE_INSTALL_PREFIX=${HOME}/.local/nvim
 
 make install
 
