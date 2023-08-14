7e33d04a2afac3b617395190b9ea3b35af620f06
uhooi <the_uhooi@yahoo.co.jp>
2023-08-14 13:30:48 +0900

    Fix ugaterm keymap

--- .config/nvim/lua/plugins/config/ugaterm.lua
+++ .config/nvim/lua/plugins/config/ugaterm.lua
@@ -2,5 +2,5 @@
 --    : https://github.com/uga-rosa/dotfiles/blob/8b112e123d9b03e20e9b193751a068432f8e0c44/nvim/rc/plugins.toml#L54-L60
 require('ugaterm').setup {}
 
-vim.keymap.set({ 'n', 't' }, '<Leader>t', '<Cmd>UgatermToggle<CR>')
+vim.keymap.set({ 'n', 't' }, '<Leader>t', '<Cmd>UgatermOpen -toggle<CR>')
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
