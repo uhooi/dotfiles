e398cee52be0932948b7fa0a41bb2892a3eece4e
uhooi <the_uhooi@yahoo.co.jp>
2023-10-07 12:42:26 +0900

    Refactor gitlinker config

--- .config/nvim/lua/plugins/config/gitlinker.lua
+++ .config/nvim/lua/plugins/config/gitlinker.lua
@@ -1,9 +1,6 @@
-local gitlinker = require('gitlinker')
-local gitlinker_actions = require('gitlinker.actions')
-
-gitlinker.setup {
+require('gitlinker').setup {
   opts = {
-    action_callback = gitlinker_actions.open_in_browser,
+    action_callback = require('gitlinker.actions').open_in_browser,
     print_url = false,
   },
   mappings = '<Leader>go',
