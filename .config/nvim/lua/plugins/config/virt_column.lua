b857b6db393f7afe1ef5a389a54c8323511cd99e
uhooi <the_uhooi@yahoo.co.jp>
2023-10-16 01:56:07 +0900

    Update virt-column config

--- .config/nvim/lua/plugins/config/virt_column.lua
+++ .config/nvim/lua/plugins/config/virt_column.lua
@@ -1,3 +1,4 @@
 require('virt-column').setup {
   char = '│', -- ref: https://github.com/lukas-reineke/indent-blankline.nvim/blob/7075d7861f7a6bbf0de0298c83f8a13195e6ec01/doc/indent_blankline.txt#L159
+  hightlight = 'IblIndent', -- ref: https://github.com/lukas-reineke/indent-blankline.nvim/blob/0fe34b4c1b926e106d105d3ae88ef6cbf6743572/doc/indent_blankline.txt#L296
 }
