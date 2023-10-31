de39cbaf318ef6043f0a0967cbd063fd52458b0d
uhooi <the_uhooi@yahoo.co.jp>
2023-11-01 01:58:38 +0900

    Update lsp-lens config

--- .config/nvim/lua/plugins/config/lsp_lens.lua
+++ .config/nvim/lua/plugins/config/lsp_lens.lua
@@ -1,3 +1,10 @@
-require('lsp-lens').setup()
+require('lsp-lens').setup {
+  sections = {
+    definition = false,
+    references = true,
+    implements = true,
+    git_authors = true,
+  },
+}
 
 vim.keymap.set('n', 'gs', '<Cmd>LspLensToggle<CR>')
