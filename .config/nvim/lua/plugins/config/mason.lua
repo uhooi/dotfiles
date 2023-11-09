11f394f43c12c4bb2899503c327c25fefd98150c
uhooi <the_uhooi@yahoo.co.jp>
2023-11-10 00:21:45 +0900

    Update lua_ls config

--- .config/nvim/lua/plugins/config/mason.lua
+++ .config/nvim/lua/plugins/config/mason.lua
@@ -53,8 +53,11 @@ mason_lspconfig.setup_handlers {
             globals = { 'vim' },
           },
           workspace = {
-            checkThirdParty = false,
-            library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = 'Disable',
+            library = {
+              vim.env.VIMRUNTIME,
+              '${3rd}/luv/library',
+            },
           },
           telemetry = {
             enable = false,
