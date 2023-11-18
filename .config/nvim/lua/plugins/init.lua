ea7bd71f768681a0025a1299dcc08421ee6b8da5
uhooi <the_uhooi@yahoo.co.jp>
2023-11-18 21:50:27 +0900

    Disable neodev.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -248,6 +248,7 @@ require('lazy').setup({
 
   {
     'folke/neodev.nvim',
+    enabled = false,
     config = function()
       require('neodev').setup {
         library = { plugins = { 'nvim-dap-ui' }, types = true },
