979ac16cbf507051b5f75f7633d1983c01c7c61e
uhooi <the_uhooi@yahoo.co.jp>
2023-11-19 09:34:42 +0900

    Move LSP config

---
+++ .config/nvim/lua/plugins/config/lsp/lua_ls.lua
@@ -0,0 +1,31 @@
+-- ref: https://github.com/neovim/nvim-lspconfig/blob/27e9e1d2638ffd133b06f314a49ac55825cb1017/doc/server_configurations.md#lua_ls
+--    : https://luals.github.io/wiki/settings/
+--    : https://github.com/uga-rosa/dotfiles/blob/f2770a2b210cfdeec43c8a7508e88929f724ac34/nvim/rc/lsp.toml#L110-L133
+return {
+  Lua = {
+    runtime = {
+      version = 'LuaJIT',
+    },
+    diagnostics = {
+      globals = { 'vim' },
+    },
+    workspace = {
+      checkThirdParty = 'Disable',
+      library = {
+        vim.env.VIMRUNTIME,
+        '${3rd}/luv/library',
+      },
+    },
+    telemetry = {
+      enable = false,
+    },
+    format = {
+      enable = false, -- Use StyLua
+    },
+    -- ref: https://github.com/LuaLS/lua-language-server/wiki/Tips#inlay-hints
+    --    : https://github.com/LuaLS/lua-language-server/wiki/Settings#hint
+    hint = {
+      enable = true,
+    },
+  },
+}
--- .config/nvim/lua/plugins/config/nvim_lspconfig.lua
+++ .config/nvim/lua/plugins/config/nvim_lspconfig.lua
@@ -44,37 +44,7 @@ mason_lspconfig.setup_handlers {
       capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     }
     if server_name == 'lua_ls' then
-      -- ref: https://github.com/neovim/nvim-lspconfig/blob/27e9e1d2638ffd133b06f314a49ac55825cb1017/doc/server_configurations.md#lua_ls
-      --    : https://luals.github.io/wiki/settings/
-      --    : https://github.com/uga-rosa/dotfiles/blob/f2770a2b210cfdeec43c8a7508e88929f724ac34/nvim/rc/lsp.toml#L110-L133
-      opts.settings = {
-        Lua = {
-          runtime = {
-            version = 'LuaJIT',
-          },
-          diagnostics = {
-            globals = { 'vim' },
-          },
-          workspace = {
-            checkThirdParty = 'Disable',
-            library = {
-              vim.env.VIMRUNTIME,
-              '${3rd}/luv/library',
-            },
-          },
-          telemetry = {
-            enable = false,
-          },
-          format = {
-            enable = false, -- Use StyLua
-          },
-          -- ref: https://github.com/LuaLS/lua-language-server/wiki/Tips#inlay-hints
-          --    : https://github.com/LuaLS/lua-language-server/wiki/Settings#hint
-          hint = {
-            enable = true,
-          },
-        },
-      }
+      opts.settings = require('plugins.config.lsp.lua_ls')
     end
     require('lspconfig')[server_name].setup(opts)
   end,
