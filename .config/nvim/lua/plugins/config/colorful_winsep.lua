d4be4431ea28b9bb86d4e02a4bc51acb7dd2bacf
uhooi <the_uhooi@yahoo.co.jp>
2023-12-04 13:14:11 +0900

    Update colorful-winsep.nvim fg

--- .config/nvim/lua/plugins/config/colorful_winsep.lua
+++ .config/nvim/lua/plugins/config/colorful_winsep.lua
@@ -1,6 +1,7 @@
 require('colorful-winsep').setup {
+  -- ref: https://github.com/gen740/SmoothCursor.nvim/blob/8c2c5d61899a5c0ecc1dc34827a6eda1270cebdf/lua/smoothcursor/utils.lua#L137
   highlight = {
-    fg = '#FF0000',
+    fg = '#FFD400',
   },
   -- ref: https://github.com/neovim/neovim/blob/df87266b23b32b96d1e3d4d26eb721a9dd63c2a4/src/nvim/api/win_config.c#L527
   symbols = { '─', '│', '╭', '╮', '╰', '╯' },
