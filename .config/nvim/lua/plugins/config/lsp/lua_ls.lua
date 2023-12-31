5b87aecf8dd39b225c7f700f0c3629cff269d6f3
uhooi <the_uhooi@yahoo.co.jp>
2023-12-31 14:55:24 +0900

    Update lua_ls.lua

--- .config/nvim/lua/plugins/config/lsp/lua_ls.lua
+++ .config/nvim/lua/plugins/config/lsp/lua_ls.lua
@@ -1,4 +1,4 @@
--- ref: https://github.com/neovim/nvim-lspconfig/blob/27e9e1d2638ffd133b06f314a49ac55825cb1017/doc/server_configurations.md#lua_ls
+--    : https://github.com/neovim/nvim-lspconfig/blob/9bedcfed749755e43fc8bed46f85ce2044fe3b77/doc/server_configurations.md#lua_ls
 --    : https://luals.github.io/wiki/settings/
 --    : https://github.com/uga-rosa/dotfiles/blob/f2770a2b210cfdeec43c8a7508e88929f724ac34/nvim/rc/lsp.toml#L110-L133
 return {
@@ -14,7 +14,9 @@ return {
       library = {
         vim.env.VIMRUNTIME,
         '${3rd}/luv/library',
-},
+        '${3rd}/busted/library',
+      },
+      -- library = vim.api.nvim_get_runtime_file('', true), -- This is a lot slower
     },
     telemetry = {
       enable = false,
