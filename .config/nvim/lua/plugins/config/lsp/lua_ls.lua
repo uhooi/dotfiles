1777f0749373ac556c742ceba343621369a6b58c
uhooi <the_uhooi@yahoo.co.jp>
2024-01-03 09:45:50 +0900

    Update lua_ls.lua

--- .config/nvim/lua/plugins/config/lsp/lua_ls.lua
+++ .config/nvim/lua/plugins/config/lsp/lua_ls.lua
@@ -1,6 +1,23 @@
 --    : https://github.com/neovim/nvim-lspconfig/blob/9bedcfed749755e43fc8bed46f85ce2044fe3b77/doc/server_configurations.md#lua_ls
 --    : https://luals.github.io/wiki/settings/
 --    : https://github.com/uga-rosa/dotfiles/blob/f2770a2b210cfdeec43c8a7508e88929f724ac34/nvim/rc/lsp.toml#L110-L133
+
+-- Functions {{{
+
+-- ref: https://zenn.dev/uga_rosa/articles/afe384341fc2e1#lazy.nvim-の場合
+---@param name string
+---@return string?
+local function get_plugin_path(name)
+  local plugins = require('lazy.core.config').plugins
+  if plugins[name] then
+    return plugins[name].dir
+  else
+    vim.notify('Invalid plugin name: ' .. name, vim.log.levels.WARN)
+  end
+end
+
+-- }}}
+
 return {
   Lua = {
     runtime = {
@@ -18,7 +35,8 @@ return {
         '${3rd}/busted/library',
         '${3rd}/luassert/library',
         '${3rd}/luv/library',
-        -- vim.fn.stdpath('data') .. '/lazy', -- Plugins -- FIXME: Not work
+        get_plugin_path('lazy.nvim'),
+        get_plugin_path('nougat.nvim'),
         vim.fn.stdpath('config'),
       },
       -- library = vim.api.nvim_get_runtime_file('', true), -- This is a lot slower
