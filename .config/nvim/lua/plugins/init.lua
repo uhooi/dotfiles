841ba600bbc7a4528c36ac3e17904a4d17b0c5a8
uhooi <the_uhooi@yahoo.co.jp>
2024-01-03 14:39:06 +0900

    Refactor lazypath

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 -- lazy.nvim
 
 -- Install {{{
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fs.joinpath(vim.fn.stdpath('data')--[[@as string]], 'lazy', 'lazy.nvim')
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
     'git',
