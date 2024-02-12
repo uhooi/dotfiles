7dbc3c7041728991ae319dd8a7973a43d2388b47
uhooi <the_uhooi@yahoo.co.jp>
2024-02-12 12:25:56 +0900

    Update nvim-dap keymaps

--- .config/nvim/lua/plugins/config/nvim_dap.lua
+++ .config/nvim/lua/plugins/config/nvim_dap.lua
@@ -1,4 +1,5 @@
 local dap = require('dap')
+local dap_ui_widgets = require('dap.ui.widgets')
 local xcodebuild_dap = require('xcodebuild.dap')
 
 dap.configurations.swift = {
@@ -27,6 +28,25 @@ dap.adapters.codelldb = {
   },
 }
 
+vim.keymap.set('n', '<F5>', dap.continue)
+vim.keymap.set('n', '<F10>', dap.step_over)
+vim.keymap.set('n', '<F11>', dap.step_into)
+vim.keymap.set('n', '<F12>', dap.step_out)
+vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint)
+vim.keymap.set('n', '<Leader>dB', function()
+  dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+end)
+vim.keymap.set('n', '<Leader>dr', dap.repl.open)
+vim.keymap.set('n', '<Leader>dl', dap.run_last)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', dap_ui_widgets.hover)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', dap_ui_widgets.preview)
+vim.keymap.set('n', '<Leader>df', function()
+  dap_ui_widgets.centered_float(dap_ui_widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  dap_ui_widgets.centered_float(dap_ui_widgets.scopes)
+end)
+
 vim.keymap.set('n', '<Leader>dd', xcodebuild_dap.build_and_debug, { desc = 'Build & Debug' })
-vim.keymap.set('n', '<Leader>dr', xcodebuild_dap.debug_without_build, { desc = 'Debug Without Building' })
+vim.keymap.set('n', '<Leader>dR', xcodebuild_dap.debug_without_build, { desc = 'Debug Without Building' })
 vim.keymap.set('n', '<Leader>dt', xcodebuild_dap.debug_tests, { desc = 'Debug Tests' })
