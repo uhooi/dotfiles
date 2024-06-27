c1e6d726b614f0448e2a39c8de635bbeda55d0a5
uhooi <the_uhooi@yahoo.co.jp>
2024-06-27 18:09:16 +0900

    Update SmoothCursor config

--- .config/nvim/lua/plugins/config/smooth_cursor.lua
+++ .config/nvim/lua/plugins/config/smooth_cursor.lua
@@ -1,4 +1,24 @@
 require('smoothcursor').setup {
-  cursor = '❯',
+  cursor = '▷',
   disable_float_win = true,
 }
+
+-- ref: https://github.com/delphinus/dotfiles/blob/2ac7f4685f4d753bddc688766ca7b8018bd52d94/.config/nvim/lua/lazies/start.lua#L96-L113
+vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+  callback = function()
+    local current_mode = vim.fn.mode()
+    if current_mode == 'n' then
+      vim.fn.sign_define('smoothcursor', { text = '▷' })
+    elseif current_mode == 'i' then
+      vim.fn.sign_define('smoothcursor', { text = '▶' })
+    elseif current_mode == 'v' then
+      vim.fn.sign_define('smoothcursor', { text = '═' })
+    elseif current_mode == 'V' then
+      vim.fn.sign_define('smoothcursor', { text = '║' })
+    elseif current_mode == '' then
+      vim.fn.sign_define('smoothcursor', { text = '╬' })
+    elseif current_mode == 'R' then
+      vim.fn.sign_define('smoothcursor', { text = '⟩' })
+    end
+  end,
+})
