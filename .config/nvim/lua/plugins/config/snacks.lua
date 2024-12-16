42747d2fdf39e7ed90c7162cec5c31d678d3b771
uhooi <the_uhooi@yahoo.co.jp>
2024-12-16 11:24:40 +0900

    Disable snacks notifier

--- .config/nvim/lua/plugins/config/snacks.lua
+++ .config/nvim/lua/plugins/config/snacks.lua
@@ -3,7 +3,7 @@ local snacks = require('snacks')
 snacks.setup {
   bigfile = { enabled = true },
   dashboard = { enabled = true },
-  notifier = { enabled = true },
+  notifier = { enabled = false }, -- Use nvim-notify
   quickfile = { enabled = true },
   statuscolumn = { enabled = true },
   words = { enabled = true },
