91b34753a926732482830a122b75403abc5d36ce
uhooi <the_uhooi@yahoo.co.jp>
2025-03-18 17:41:58 +0900

    Update snacks config

--- .config/nvim/lua/plugins/config/snacks.lua
+++ .config/nvim/lua/plugins/config/snacks.lua
@@ -3,8 +3,14 @@ local snacks = require('snacks')
 snacks.setup {
   bigfile = { enabled = true },
   dashboard = { enabled = true },
+  explorer = { enabled = true },
+  indent = { enabled = true },
+  input = { enabled = true },
+  picker = { enabled = true },
   notifier = { enabled = false }, -- Use nvim-notify
   quickfile = { enabled = true },
+  scope = { enabled = true },
+  scroll = { enabled = true },
   statuscolumn = { enabled = true },
   words = { enabled = true },
 }
