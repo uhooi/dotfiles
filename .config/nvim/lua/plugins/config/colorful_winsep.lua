e7001e48724d5d9972631d11a9db2879e5726131
uhooi <the_uhooi@yahoo.co.jp>
2026-07-28 17:04:07 +0900

    Update colorful-winsep.nvim config

--- .config/nvim/lua/plugins/config/colorful_winsep.lua
+++ .config/nvim/lua/plugins/config/colorful_winsep.lua
@@ -1,10 +1,9 @@
 local color = require('plugins.config.shared.color')
 
 require('colorful-winsep').setup {
-  -- ref: https://github.com/gen740/SmoothCursor.nvim/blob/8c2c5d61899a5c0ecc1dc34827a6eda1270cebdf/lua/smoothcursor/utils.lua#L137
-  highlight = {
-    fg = color.accent,
+  border = 'rounded',
+  indicator_for_2wins = {
+    position = false,
   },
-  -- ref: https://github.com/neovim/neovim/blob/df87266b23b32b96d1e3d4d26eb721a9dd63c2a4/src/nvim/api/win_config.c#L527
-  symbols = { '─', '│', '╭', '╮', '╰', '╯' },
+  colors = color.accent,
 }
